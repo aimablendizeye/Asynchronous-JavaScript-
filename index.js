84627fa68156a39c17b342b64c89ab9a1958f18d
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-10 10:05:47 +0200

    Timer

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 
-function useName (name){
-    console.log(name);
-}
+// function useName (name){
+//     console.log(name);
+// }
+// function  greet (say){
+//     console.log(say);
 
-function  greet (say){
-    console.log(say);
-}
+// }
+// setTimeout (() => {
+//     console.log("good Bye")
+// },1000);
 
-greet("Good Morning");
-useName("Aimable");
+// greet("Good Morning");
+// useName("Aimable");
+
+// function greeting (callback) {
+//     console.log("Good Morning Aimable");
+
+// } 
+
+// function bye (){
+//     console.log("Thank you for your time");
+// }
+
+// greeting(bye()
+
+setTimeout(() => {
+    console.log("hey Aimable");
+},1000);
