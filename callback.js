6d68813db091f5a7ff684a76437ce93800ebcb48
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-17 10:03:47 +0200

    second example on the callback

--- callback.js
+++ callback.js
@@ -4,9 +4,19 @@ function calculate (a,b,callback){
     callback();
 } 
 
-function message () {
+function message() {
     console.log("calculation is done");
 }
 
 // console.log(calculate(2,3,message()));
 calculate(2,3,message);
+
+
+function greet(name, callback) {
+  console.log("Hello " + name);
+  callback();
+}
+
+greet("John", function() {
+  console.log("Welcome!");
+});
