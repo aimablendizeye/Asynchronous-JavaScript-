a3fc51ca7d19dcc8035e99c6e8643d668a030697
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-19 10:34:45 +0200

    Trying to convert some data

--- fetch.js
+++ fetch.js
@@ -16,27 +16,40 @@
 
 
 
-async function loadData() {
-  const response = await fetch("http://localhost:3000/aimable.json");
 
-  const data = await response.json();
 
-  console.log(data.user.name);
-}
+// async function loadData() {
+//   const response = await fetch("http://localhost:3000/aimable.json");
 
-loadData();
+//   const data = await response.json();
+
+//   console.log(data.user.name);
+// }
+
+// loadData();
 
 
 
 
 // Fetching multiple files 
 
+
+
 // async function multiple (){
 
 //     const  [customerData,productData,salesData] = await Promise.all([
-              
+//               fetch("customer.json"), 
+//               fetch("product.json"),
+//               fetch ("sales.json")
+                      
 //     ])
 
-    
+      
 // }
 
+
+
+const name = ["Aimable","Benjamin","Reponse","Manzi"]
+
+const stringfy = JSON.stringify(name);
+console.log(name);
