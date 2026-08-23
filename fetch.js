5d87fb33a88d1491402f07537c452725667bd25d
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-23 19:31:01 +0200

    Pikochu fetch

--- fetch.js
+++ fetch.js
@@ -278,5 +278,29 @@ const render = (users) => {
 
 
 
+console.log("JavaScript file loaded");
+
+console.log("About to run fetch");
+
+
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+  
+
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Fetch error:", error);
+  });
+
+
+   
+
 
   
