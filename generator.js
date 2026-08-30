f7a9e894dfbb4581eb73c8d84543dba1943b8c04
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-30 22:31:01 +0200

    generator demo

--- generator.js
+++ generator.js
@@ -31,14 +31,40 @@
 // Generators are also iterables 
 
 
-async function* giveOut () {
-    yield "a";
-    yield "b";
-    yield "c";
-    yield "d";
-}
-//const Generators  = giveOut();
+// async function* giveOut () {
+//     yield "a";
+//     yield "b";
+//     yield "c";
+//     yield "d";
+// }
+// //const Generators  = giveOut();
 
-for await (let letters of giveOut()) {
-    console.log(letters.toUpperCase());
-}
+// for await (let letters of giveOut()) {
+//     console.log(letters.toUpperCase());
+// }
+
+
+
+// Streams (e.g., ReadableStream): A low-level browser or Node.js API designed to handle large chunks of data (like files, videos, or network payloads) bytes-by-bytes without loading everything into memory.
+
+// Async Iterators: A JavaScript language protocol. It is just an object with a .next() method, but instead of returning a value immediately, .next() returns a Promise that resolves to { value, done }.
+
+// for await...of: A high-level modern JavaScript loop syntax. It is specifically designed to automatically pause and wait for each Promise yielded by an Async Iterator to resolve before moving to the next iteration.
+
+
+
+const products = [
+  { name: "Laptop", price: 1200 },
+  { name: "Phone", price: 800 },
+  { name: "Keyboard", price: 100 }
+];
+
+
+const first = products.filter (product => product.name === "Laptop");
+
+console.log(first);
+// for (const product of products) {
+
+
+//   console.log(product.name);
+// }
