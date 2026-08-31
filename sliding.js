08ed1ef6b902ecc3ee9fcc73029d8ae68095c714
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-08-31 11:25:14 +0200

    Lowest subArray to give a target

--- sliding.js
+++ sliding.js
@@ -1,13 +1,42 @@
 
 
-function nested (arr,k) {
-     for (let i=0; i<=arr.length-k; i++) {
-        
-        for (let j=0; j<k; j++) {
-            console.log(arr[i + j]);
-        }
-     }
+// function maxSubarraySum(arr, k) {
+//   let windowSum = 0;
+//   let maxSum = 0;
 
+//   // Calculate the first window
+//   for (let i = 0; i < k; i++) {
+//     windowSum += arr[i];
+//   }
+
+//   maxSum = windowSum;
+
+//   // Slide the window
+//   for (let i = k; i < arr.length; i++) {
+//     windowSum -= arr[i - k]; // remove left element
+//     windowSum += arr[i];     // add new right element
+
+//     maxSum = Math.max(maxSum, windowSum);
+//   }
+
+//   return maxSum;
+// }
+// console.log(maxSubarraySum([1,2,3,4,5,6,7,8],3));
+
+
+function lowestSubarray (arr, target) {
+
+  let sum=0;
+  let lowestSub = 0
+
+  for (let i=0; i<arr.length; i++) {
+    sum += arr[i];
+    lowestSub++;
+    if (sum >= target) {
+      break;
+    }
+  }
+  return lowestSub;
 }
 
-nested([1,2,3,4,5,6,7,8],3);
+console.log(lowestSubarray([2,3,4,5,6,7],16));
