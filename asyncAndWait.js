2e3165cd76238bbdde1fad1d895faffee6f42b50
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-07 15:29:01 +0200

    asyncandawait

--- asyncAndWait.js
+++ asyncAndWait.js
@@ -1,1 +1,45 @@
 
+
+// async function asynCount() {
+       
+//       for (let i=0; i<5; i++) {
+//     await  setTimeout (() =>{
+    
+//            let data = i; 
+//            return data;
+//     },1000)
+     
+//       }
+// }
+
+// asynCount()
+//           .then(data => console.log(data))
+//           .catch(error => console.log(error))
+
+
+
+
+// Q 6 from 
+
+
+function delay(ms) {
+    return new Promise(resolve => {
+        setTimeout (resolve,ms)
+    }) 
+}
+
+async function gettingCount() {
+
+    let numbers = [];
+     for (let i=0; i<5; i++) {
+      await delay(1000)
+
+      console.log(i)
+      numbers.push(i);        
+          }
+          return numbers;
+    
+}
+gettingCount ()
+              .then(numbers => console.log(numbers))
+              .catch (error => console.log(error));
