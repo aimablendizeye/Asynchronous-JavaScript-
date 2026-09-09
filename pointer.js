34bceabfbb71f66f9034c7bb678148ced5b7db2a
Aimable Ndizeye <nbigaimable025@gmail.com>
2026-09-09 15:21:06 +0200

    Testing

---
+++ pointer.js
@@ -0,0 +1,8 @@
+
+function information (name, city , temp) {
+    console.log(`country: ${name}`)
+     console.log(`capital: ${city}`)
+      console.log(`temp: ${temp}`)
+}
+
+information("Rwanda","Kigali",23.6);
